aa2a68a3f67394215977ce96ece477fd9c8efce0
Faisal Mumtaz <fascaptech@gmail.com>
2024-02-06 10:46:29 +0000

    Feat : Add components

---
+++ client/src/Misdemeanour.tsx
@@ -0,0 +1,13 @@
+// Misdemeanour.tsx
+import React from 'react';
+
+const Misdemeanour: React.FC = () => {
+    
+    return(
+        <div>
+            <h1>Misdemeanour Page!</h1>
+            <p>Confess your sins here!</p>
+        </div>
+    );
+};
+export default Misdemeanour;
---
+++ client/src/components/Confession.tsx
@@ -0,0 +1,13 @@
+// Confession.tsx
+
+const Confession: React.FC = () => {
+return(
+  <div>
+    <h1>Confession Page!</h1>
+    <p>Confess your sins here!</p>
+  </div>
+);
+};
+
+
+export default Confession;
